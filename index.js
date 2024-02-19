c80594c882a8bc9f1522943c8a31a3054719c16f
DavudNebizade <137636302+DavudNebizade@users.noreply.github.com>
2024-02-19 19:32:11 +0300

    project6

---
+++ index.js
@@ -0,0 +1,17 @@
+let ad = prompt('Adinizi yazin')
+let sifre = prompt('Adinizi yazin')
+
+switch (sifre) {
+    case registrator:
+        cavab = 'Xos geldin Erlams'
+        break;
+    case registrator:
+        cavab = 'Istifadeci adi ve ya sifre yanlisdir'
+        break;
+    case registrator:
+        cavab = 'Admin,Siz sayti redakte ede bilersiniz'
+        break;
+    default:
+        console.log();
+        break;
+}
